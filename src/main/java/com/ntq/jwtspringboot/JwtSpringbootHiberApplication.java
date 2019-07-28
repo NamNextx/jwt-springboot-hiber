d39bfe90dbc4085de3a4edafa3d53288f3bdb1cd
NamTruong <nam.truong.intern@ntq-solution.com.vn>
2019-07-28 22:18:40 +0700

    Initial commit

---
+++ src/main/java/com/ntq/jwtspringboot/JwtSpringbootHiberApplication.java
@@ -0,0 +1,13 @@
+package com.ntq.jwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringbootHiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringbootHiberApplication.class, args);
+	}
+
+}
